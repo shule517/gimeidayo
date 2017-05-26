a353c50434fe3572eacc28ea59deb81abbe7337c
シュール <shule517@gmail.com>
2017-05-27 00:14:50 +0900

    gimeiを表示するようにした

--- app/controllers/gimei_controller.rb
+++ app/controllers/gimei_controller.rb
@@ -1,4 +1,9 @@
 class GimeiController < ApplicationController
   def index
+    @names = 100.times.map do
+      person = Gimei.new
+      name = person.name
+      "#{name.male? ? '男' : '女'} ： #{name} (#{name.hiragana})"
+    end
   end
 end
