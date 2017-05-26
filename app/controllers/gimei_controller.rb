f22be38b877d899e79a3146fcc199f084128cea5
シュール <shule517@gmail.com>
2017-05-27 00:18:59 +0900

    gimeiの表示位置を変更

--- app/controllers/gimei_controller.rb
+++ app/controllers/gimei_controller.rb
@@ -1,6 +1,6 @@
 class GimeiController < ApplicationController
   def index
-    @names = 100.times.map do
+    @names = 15.times.map do
       person = Gimei.new
       name = person.name
       "#{name.male? ? '男' : '女'} ： #{name} (#{name.hiragana})"
